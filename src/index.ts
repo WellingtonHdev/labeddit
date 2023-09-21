e1f0fdcbbb4bd30405cf10e651aae6a48c3ab509
WellingtonHdev <wellingtontmv@gmail.com>
2023-09-21 19:08:26 +0100

    modelagem backend

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import authRoutes from './routes/auth';
 import postsRoutes from './routes/posts';
 import { Database } from 'better-sqlite3';
 import { createUser } from './models/Users'
 import { createPost } from './models/Post';
 
+dotenv.config()
+
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/posts', postsRoutes);
 
-const PORT = process.env.PORT || 3001;
+app.listen(Number(process.env.PORT) || 3003, () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
+})
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+app.get("/ping", (req, res) => {
+    res.send("pong!")
+})
 
